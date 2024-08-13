8db9eee537b1f3e553ba66c4b9af2facf329927f
Andreas Zeissner <andi@wundergraph.com>
2024-08-13 15:59:17 +0200

    feat: test validity harden commit walk and add cosmo-ops test

--- chglog/chglog.go
+++ chglog/chglog.go
@@ -1,12 +1,12 @@
 package chglog
 
 import (
-	"fmt"
 	"log"
 	"os"
 	"strings"
 
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/go-git/go-git/v5/plumbing/object"
 	"github.com/urfave/cli/v2"
 	"gopkg.in/yaml.v3"
@@ -16,6 +16,18 @@ const (
 	StopIteration = "StopIteration"
 )
 
+func filterSubirs(directories []string) func(string) bool {
+	return func(path string) bool {
+		var ok = false
+		for _, dir := range directories {
+			if strings.HasPrefix(path, dir) {
+				ok = true
+				break
+			}
+		}
+		return ok
+	}
+}
 func GenerateChangelogForRepo(from, to, repoURL string, subdirectories []string) []*ArtifactHubChangelogObject {
 	chglogs := []*ArtifactHubChangelogObject{}
 	repo := OpenRepo(repoURL)
@@ -23,56 +35,61 @@ func GenerateChangelogForRepo(from, to, repoURL string, subdirectories []string)
 	_, fromCommit := ResolveTag(repo, from)
 	_, toCommit := ResolveTag(repo, to)
 
-	commitIter, err := repo.Log(&git.LogOptions{
-		From: fromCommit.Hash,
-	})
-	if err != nil {
-		log.Fatalf("Failed to get commit iterator: %v", err)
+	options := &git.LogOptions{
+		From: toCommit.Hash,
+		All:  false,
 	}
 
-	err = commitIter.ForEach(func(c *object.Commit) error {
-		if c.Hash == toCommit.Hash {
-			return fmt.Errorf(StopIteration)
+	if len(subdirectories) > 0 {
+		options = &git.LogOptions{
+			From:       toCommit.Hash,
+			All:        false,
+			PathFilter: filterSubirs(subdirectories),
 		}
-		if len(subdirectories) > 0 {
-			parent, err := c.Parent(0)
-			if err != nil {
-				return err
-			}
+	}
+	// Initialize a map to store commits reachable from the fromCommit
+	visited := make(map[plumbing.Hash]bool)
 
-			patch, err := c.Patch(parent)
-			if err != nil {
-				return err
-			}
-			for _, fileStat := range patch.Stats() {
-				for _, subdir := range subdirectories {
-					if strings.HasPrefix(fileStat.Name, subdir) {
-						resolver := NewConventionalCommitsResolver(c)
-						chglog, err := resolver.ResolveChangelogEntry()
-						if err != nil {
-							return nil
-						}
-						chglogs = append(chglogs, chglog)
-						return nil
-					}
-				}
-			}
-		} else {
-			resolver := NewConventionalCommitsResolver(c)
-			chglog, err := resolver.ResolveChangelogEntry()
-			if err != nil {
-				return nil
-			}
-			chglogs = append(chglogs, chglog)
+	// Walk from `fromCommit` to mark all reachable commits
+	err := walkCommits(repo, fromCommit, visited)
+	if err != nil {
+		log.Fatalf("Failed to walk commits: %v", err)
+	}
+
+	// Now iterate from `toCommit` and only print commits not in `visited`
+	iter, err := repo.Log(options)
+	if err != nil {
+		log.Fatalf("Log commits: %v", err)
+	}
+
+	defer iter.Close()
+	err = iter.ForEach(func(c *object.Commit) error {
+		// Stop if we reach the `fromCommit`
+		if visited[c.Hash] {
+			return nil
 		}
+
+		resolver := NewConventionalCommitsResolver(c)
+		chglog, err := resolver.ResolveChangelogEntry()
+		if err != nil {
+			return nil
+		}
+		chglogs = append(chglogs, chglog)
 
 		return nil
 	})
-	if err != nil && err.Error() != StopIteration {
-		log.Fatalf("Error iterating commits: %v", err)
-	}
 
 	return chglogs
+}
+
+// walkCommits walks through the commit history starting from `commit` and marks all reachable commits
+func walkCommits(repo *git.Repository, commit *object.Commit, visited map[plumbing.Hash]bool) error {
+	commitIter := object.NewCommitPreorderIter(commit, nil, nil)
+	err := commitIter.ForEach(func(c *object.Commit) error {
+		visited[c.Hash] = true
+		return nil
+	})
+	return err
 }
 
 func NewCli() cli.ActionFunc {
